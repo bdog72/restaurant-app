1aa6fbd98809044b9d509e4c05fd7b621ffe454b
Brian Bycynski <high5bri@hotmail.com>
2017-02-23 08:16:40 -0500

    added new Linkcontainer from react-Bootstrap

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,6 @@
 import React, {Component} from 'react'
 import { PageHeader } from 'react-bootstrap'
 import Thumbnails from './Thumbnails'
-// import {Menu} from 'semantic-ui-react'
-// import {Link} from 'react-router'
 import { LinkContainer } from 'react-router-bootstrap'
 export default class Layout extends Component {
   static propTypes = {
@@ -13,7 +11,6 @@ export default class Layout extends Component {
     return <div>
       <LinkContainer to='/'>
         <PageHeader bsClass='head'>
-          {/* <Menu.Item as={Link} to='/'><p>Food-Dot-Com</p></Menu.Item> */}
           <p>Food-Dot-Com</p>
         </PageHeader>
       </LinkContainer>
